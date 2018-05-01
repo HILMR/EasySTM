13db7d48936d2c1ee23e713ea86da9cc6db5b026
HILMR <38877234+HILMR@users.noreply.github.com>
2018-05-01 15:11:16 +0800

    EasySTM32 for F103
    V1.0 byLMR
    Date 2018-4-20

---
+++ EasySTM32F103.c
@@ -0,0 +1,686 @@
+/*
+	【EasySTM32F103库函数说明】
+	本库集成封装了STM32F103系列单片机多种基本功能，简化设置，易于入门，
+	贴近Arduino的编程风格，尤其适合从Arduino进阶进入STM32的学习
+	主要功能有：数字输出、上下拉数字输入、模拟输入、PWM输出、串口通信、
+	数据类型转换、外部中断、定时器中断
+	
+作者：LMR
+版本号：V1.0
+创建日期：2018-4-20
+
+*/
+
+#include "EasySTM32F103.h"
+#include "delay.h"
+
+#define SerialMAXBUF 32 //串口最大数据接收大小（字节）
+
+GPIO_TypeDef* getGPIOX(u16 GPIO_Pin)
+{
+	switch ((int)(GPIO_Pin/100))
+	{
+		case 1:return GPIOA;break;
+		case 2:return GPIOB;break;
+		case 3:return GPIOC;break;
+		case 4:return GPIOD;break;
+		case 5:return GPIOE;break;
+		case 6:return GPIOF;break;
+		case 7:return GPIOG;break;
+	}
+}
+
+u16 getGPIOP(u16 GPIO_Pin)
+{
+	switch(GPIO_Pin-(int)(GPIO_Pin/100)*100)
+	{
+		case 0:return GPIO_Pin_0;break;
+		case 1:return GPIO_Pin_1;break;
+		case 2:return GPIO_Pin_2;break;
+		case 3:return GPIO_Pin_3;break;
+		case 4:return GPIO_Pin_4;break;
+		case 5:return GPIO_Pin_5;break;
+		case 6:return GPIO_Pin_6;break;
+		case 7:return GPIO_Pin_7;break;
+		case 8:return GPIO_Pin_8;break;
+		case 9:return GPIO_Pin_9;break;
+		case 10:return GPIO_Pin_10;break;
+		case 11:return GPIO_Pin_11;break;
+		case 12:return GPIO_Pin_12;break;
+		case 13:return GPIO_Pin_13;break;
+		case 14:return GPIO_Pin_14;break;
+		case 15:return GPIO_Pin_15;break;
+	}
+}
+
+TIM_TypeDef* getPWMTIM(u16 GPIO_Pin)
+{
+	if((GPIO_Pin==108)||(GPIO_Pin==109)||(GPIO_Pin==110)||(GPIO_Pin==111))
+		{
+			return TIM1;
+		}
+		
+	if((GPIO_Pin==100)||(GPIO_Pin==101)||(GPIO_Pin==102)||(GPIO_Pin==103))
+		{
+			return TIM2;
+		}
+	if((GPIO_Pin==106)||(GPIO_Pin==107)||(GPIO_Pin==200)||(GPIO_Pin==201))//PB4无法PWM
+		{
+			return TIM3;
+		}
+	if((GPIO_Pin==206)||(GPIO_Pin==207)||(GPIO_Pin==208)||(GPIO_Pin==209))
+		{
+			return TIM4;
+		}
+}
+
+u8 getPWMCHN(u16 GPIO_Pin)
+{
+	if((GPIO_Pin==100)||(GPIO_Pin==106)||(GPIO_Pin==206)||(GPIO_Pin==108)) 
+	{
+		return 1;
+	}
+	if((GPIO_Pin==101)||(GPIO_Pin==107)||(GPIO_Pin==207)||(GPIO_Pin==205)||(GPIO_Pin==109)) 
+	{
+		return 2;
+	}
+	if((GPIO_Pin==102)||(GPIO_Pin==200)||(GPIO_Pin==208)||(GPIO_Pin==110))
+	{
+		return 3;
+	}
+	if((GPIO_Pin==103)||(GPIO_Pin==201)||(GPIO_Pin==209)||(GPIO_Pin==111)) 
+	{
+		return 4;
+	}
+}
+
+void pinMode(u16 GPIO_Pin,u8 GPIO_Mode)
+{	
+	ADC_InitTypeDef ADC_InitStructure;
+	GPIO_InitTypeDef  GPIO_InitStructure;
+	
+	switch ((int)(GPIO_Pin/100))
+	{
+		case 1:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);break;
+		case 2:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);break;
+		case 3:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);break;
+		case 4:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);break;
+		case 5:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);break;
+		case 6:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOF, ENABLE);break;
+		case 7:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG, ENABLE);break;
+	}
+	
+	GPIO_InitStructure.GPIO_Pin=getGPIOP(GPIO_Pin);
+	
+	switch(GPIO_Mode)
+	{
+		case 1:GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;break;
+		case 2:GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;break;
+		case 3:GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;break;
+		case 4:GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;break;
+	}
+	
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 //IO口速度为50MHz
+	GPIO_Init(getGPIOX(GPIO_Pin), &GPIO_InitStructure);	
+	
+	if (GPIO_Mode==4)
+	{
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1	, ENABLE );
+		RCC_ADCCLKConfig(RCC_PCLK2_Div6);
+		ADC_DeInit(ADC1);  //复位ADC1 
+		
+		ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;	//ADC工作模式:ADC1和ADC2工作在独立模式
+		ADC_InitStructure.ADC_ScanConvMode = DISABLE;	//模数转换工作在单通道模式
+		ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;	//模数转换工作在单次转换模式
+		ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;	//转换由软件而不是外部触发启动
+		ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;	//ADC数据右对齐
+		ADC_InitStructure.ADC_NbrOfChannel = 16;	//顺序进行规则转换的ADC通道的数目
+		ADC_Init(ADC1, &ADC_InitStructure);	//根据ADC_InitStruct中指定的参数初始化外设ADCx的寄存器   
+		
+		ADC_Cmd(ADC1, ENABLE);	//使能指定的ADC1
+		ADC_ResetCalibration(ADC1);	//使能复位校准  
+		while(ADC_GetResetCalibrationStatus(ADC1));	//等待复位校准结束
+		ADC_StartCalibration(ADC1);	 //开启AD校准
+		while(ADC_GetCalibrationStatus(ADC1));
+	}
+ 
+	
+}
+
+void digitalWrite(u16 GPIO_Pin,u8 DW_Mode)
+{
+	switch(DW_Mode)
+	{
+		case 0:GPIO_ResetBits(getGPIOX(GPIO_Pin),getGPIOP(GPIO_Pin));break;
+		case 1:GPIO_SetBits(getGPIOX(GPIO_Pin),getGPIOP(GPIO_Pin));break;
+	}
+}
+
+u8 digitalRead(u16 GPIO_Pin)
+{
+	return GPIO_ReadInputDataBit(getGPIOX(GPIO_Pin),getGPIOP(GPIO_Pin));
+}
+
+
+void setPWM(u16 GPIO_Pin,u16 PWM_MAX,u8 PWM_CMP)
+{
+	//结构体定义
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
+	
+	//初始化GPIO
+	switch ((int)(GPIO_Pin/100))
+	{
+		case 1:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);break;
+		case 2:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);break;
+		case 3:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);break;
+		case 4:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);break;
+		case 5:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);break;
+		case 6:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOF, ENABLE);break;
+		case 7:RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG, ENABLE);break;
+	}
+	
+	if(GPIO_Pin==205)
+	{//此复用功能待修正，区分部分重映射和完全重映射
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+		GPIO_PinRemapConfig(GPIO_PartialRemap_TIM3, ENABLE);
+	}
+
+	
+	GPIO_InitStructure.GPIO_Pin = getGPIOP(GPIO_Pin);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(getGPIOX(GPIO_Pin), &GPIO_InitStructure);
+	
+	//初始化定时器
+		
+	TIM_TimeBaseStructure.TIM_Period = PWM_MAX; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值
+	TIM_TimeBaseStructure.TIM_Prescaler =0; //设置用来作为TIMx时钟频率除数的预分频值，目前采用【不分频】
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //设置时钟分割:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM向上计数模式
+	
+	
+	//使能定时器
+	if(getPWMTIM(GPIO_Pin)==TIM1){RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1 , ENABLE);TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);}
+	if(getPWMTIM(GPIO_Pin)==TIM2){RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 , ENABLE);TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);}
+	if(getPWMTIM(GPIO_Pin)==TIM3){RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 , ENABLE);TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);}
+	if(getPWMTIM(GPIO_Pin)==TIM4){RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4 , ENABLE);TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);}
+			
+		
+	//初始化PWM模式
+	if (PWM_CMP==1){TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;} //选择定时器模式:TIM脉冲宽度调制模式1
+	if (PWM_CMP==0){TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;} //选择定时器模式:TIM脉冲宽度调制模式1
+	
+ 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //比较输出使能
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; //输出极性:TIM输出比较极性高
+	
+	switch(getPWMCHN(GPIO_Pin))
+	{
+		case 1:TIM_OC1Init(getPWMTIM(GPIO_Pin), &TIM_OCInitStructure);TIM_OC1PreloadConfig(getPWMTIM(GPIO_Pin), TIM_OCPreload_Enable);break;
+		case 2:TIM_OC2Init(getPWMTIM(GPIO_Pin), &TIM_OCInitStructure);TIM_OC2PreloadConfig(getPWMTIM(GPIO_Pin), TIM_OCPreload_Enable);break;
+		case 3:TIM_OC3Init(getPWMTIM(GPIO_Pin), &TIM_OCInitStructure);TIM_OC3PreloadConfig(getPWMTIM(GPIO_Pin), TIM_OCPreload_Enable);break;
+		case 4:TIM_OC4Init(getPWMTIM(GPIO_Pin), &TIM_OCInitStructure);TIM_OC4PreloadConfig(getPWMTIM(GPIO_Pin), TIM_OCPreload_Enable);break;
+	}
+	
+	TIM_Cmd(getPWMTIM(GPIO_Pin), ENABLE);
+	
+	if (getPWMTIM(GPIO_Pin)==TIM1){TIM_CtrlPWMOutputs(TIM1, ENABLE); }//高级定时器功能
+}
+
+void analogWrite(u16 GPIO_Pin,u16 PWM_VALUE)
+{
+	switch(getPWMCHN(GPIO_Pin))
+	{
+		case 1: TIM_SetCompare1(getPWMTIM(GPIO_Pin),PWM_VALUE);break;
+		case 2: TIM_SetCompare2(getPWMTIM(GPIO_Pin),PWM_VALUE);break;
+		case 3: TIM_SetCompare3(getPWMTIM(GPIO_Pin),PWM_VALUE);break;
+		case 4: TIM_SetCompare4(getPWMTIM(GPIO_Pin),PWM_VALUE);break;
+	}
+}
+
+u16 ADCRead(u16 GPIO_Pin)   
+{
+	u8 ADCCHN;
+	switch(GPIO_Pin)
+	{
+		case 100: ADCCHN=ADC_Channel_0;break;
+		case 101: ADCCHN=ADC_Channel_1;break;
+		case 102: ADCCHN=ADC_Channel_2;break;
+		case 103: ADCCHN=ADC_Channel_3;break;
+		case 104: ADCCHN=ADC_Channel_4;break;
+		case 105: ADCCHN=ADC_Channel_5;break;
+		case 106: ADCCHN=ADC_Channel_6;break;
+		case 107: ADCCHN=ADC_Channel_7;break;
+		case 200: ADCCHN=ADC_Channel_8;break;
+		case 201: ADCCHN=ADC_Channel_9;break;
+		case 300: ADCCHN=ADC_Channel_10;break;
+		case 301: ADCCHN=ADC_Channel_11;break;
+		case 302: ADCCHN=ADC_Channel_12;break;
+		case 303: ADCCHN=ADC_Channel_13;break;
+		case 304: ADCCHN=ADC_Channel_14;break;
+		case 305: ADCCHN=ADC_Channel_15;break;
+	}
+  	//设置指定ADC的规则组通道，一个序列，采样时间
+	ADC_RegularChannelConfig(ADC1, ADCCHN,1, ADC_SampleTime_239Cycles5 );	//ADC1,ADC通道,采样时间为239.5周期	  			    
+
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);		//使能指定的ADC1的软件转换启动功能	
+	 
+	while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC ));//等待转换结束
+
+	return ADC_GetConversionValue(ADC1);	//返回最近一次ADC1规则组的转换结果
+}
+
+u16 analogRead(u16 GPIO_Pin)  
+{
+	float temp_val=0;
+	u8 t;
+	u8 tt=0;
+	for(t=0;t<10;t++)
+	{
+		u32 temp=ADCRead(GPIO_Pin);
+		//均值滤波
+		if (temp>4096){tt+=1;continue;}
+		if ((t>0)&&(temp>(temp_val/(t-tt)*1.5))){tt+=1;continue;}
+		if ((t>0)&&(temp<(temp_val/(t-tt)*0.5))){tt+=1;continue;}
+		
+		temp_val+=temp;
+		delay_ms(5);
+	}
+	return temp_val/4096*1024/(10-tt);
+} 
+
+void setSerial(u8 SerialID,u32 bound)
+{
+	GPIO_InitTypeDef GPIO_STX;
+	GPIO_InitTypeDef GPIO_SRX;
+	USART_InitTypeDef USART_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	GPIO_TypeDef* GPIO_GRP;
+	USART_TypeDef* USARTx;
+
+
+	switch (SerialID)
+	{
+		case 1:{
+			RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1|RCC_APB2Periph_GPIOA, ENABLE);
+			GPIO_STX.GPIO_Pin=GPIO_Pin_9;
+			GPIO_SRX.GPIO_Pin=GPIO_Pin_10;
+			GPIO_GRP=GPIOA;
+			NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+			USARTx=USART1;
+			break;
+		}
+		
+		case 2:{
+			RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+			RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+			GPIO_STX.GPIO_Pin=GPIO_Pin_2;
+			GPIO_SRX.GPIO_Pin=GPIO_Pin_3;
+			GPIO_GRP=GPIOA;
+			NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+			USARTx=USART2;
+			break;
+		}
+		case 3:{
+			RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+			RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+			GPIO_STX.GPIO_Pin=GPIO_Pin_10;
+			GPIO_SRX.GPIO_Pin=GPIO_Pin_11;
+			GPIO_GRP=GPIOB;
+			NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+			USARTx=USART3;
+			break;
+		}
+		
+	}
+	
+	GPIO_STX.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_SRX.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_STX.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_SRX.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIO_GRP, &GPIO_STX);
+	GPIO_Init(GPIO_GRP, &GPIO_SRX);
+
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0 ;//抢占优先级0
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;		//子优先级0
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
+	NVIC_Init(&NVIC_InitStructure);
+	
+	USART_InitStructure.USART_BaudRate = bound;//串口波特率
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;//无奇偶校验位
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
+	
+	USART_Init(USARTx, &USART_InitStructure); //初始化串口
+	USART_ITConfig(USARTx,USART_IT_RXNE,ENABLE);//开启接收中断
+  USART_Cmd(USARTx, ENABLE);                    //使能串口
+	
+}
+
+void Serialwrite(u8 SerialID,char Data)
+{
+	switch(SerialID)
+	{
+		case 1:USART_SendData(USART1,Data);while(USART_GetFlagStatus(USART1,USART_FLAG_TC)==RESET){};break;
+		case 2:USART_SendData(USART2,Data);while(USART_GetFlagStatus(USART2,USART_FLAG_TC)==RESET){};break;
+		case 3:USART_SendData(USART3,Data);while(USART_GetFlagStatus(USART3,USART_FLAG_TC)==RESET){};break;
+		
+	}
+	
+}
+
+void Serialprint(u8 SerialID,char* SData)
+{
+
+	while(0!=*SData)
+		{
+			Serialwrite(SerialID,*SData);
+			SData++;
+		}
+}
+
+char* int2str(u32 oriint)
+{
+	char buf[32],*temp;//【警告】必须对指针进行赋值，否则使用sprintf后将会进入死循环
+	temp=buf;
+	sprintf(buf,"%d",oriint);
+	return temp;
+}
+
+char* flo2str(float oriflo)
+{
+	char buf[32],*temp;
+	temp=buf;
+	sprintf(buf,"%f",oriflo);
+	return temp;
+}
+
+u32 stringtoint(char* str)
+{
+	char* ori=str;//记录初始指针地址
+	u32 temp=0;
+	u8 t=0;
+	u32 ttemp;
+	u8 tt;
+	while(0!=*str)
+		{
+			t+=1;
+			str++;
+		}
+		str=ori;//指针归位
+		while(0!=*str)
+		{
+			t-=1;//一定注意，如果不减法就会逆序输出数字
+			ttemp=*str-'0';
+			tt=0;
+			while(tt!=t)
+			{
+				tt+=1;
+				ttemp=ttemp*10;
+			}
+			temp=temp+ttemp;
+			str++;
+		}	
+	return temp;
+}
+
+u8 u1_res[SerialMAXBUF];//串口数据缓存池
+u8 u2_res[SerialMAXBUF];
+u8 u3_res[SerialMAXBUF];
+u8 u1_flg=0;//起始位标记
+u8 u2_flg=0;
+u8 u3_flg=0;
+u8 u1_bufid=0;//读取数据编号
+u8 u2_bufid=0;
+u8 u3_bufid=0;
+
+void USART1_IRQHandler(void)
+{
+	 
+	u1_res[u1_bufid]=USART_ReceiveData(USART1);//接收数据
+	 
+	 if (USART_ReceiveData(USART1)==u1_flg)//辨别数据的有效性
+	 {
+		  for(u1_bufid=0;u1_bufid<SerialMAXBUF;u1_bufid++)
+	 {
+		 u1_res[u1_bufid]=0;
+	 }
+	 u1_bufid=0;
+	 /*包含起始位标记
+	 u1_bufid=1;//数据归位清除
+	 u1_res[0]=USART_ReceiveData(USART1);
+	 */
+	 }
+	 else
+	 {
+	 u1_bufid++;
+	 }
+ }
+
+void USART2_IRQHandler(void)
+{
+	 
+	u2_res[u2_bufid]=USART_ReceiveData(USART2);//接收数据
+	 
+	 if (USART_ReceiveData(USART2)==u2_flg)//辨别数据的有效性
+	 {
+		  for(u2_bufid=0;u2_bufid<SerialMAXBUF;u2_bufid++)
+	 {
+		 u2_res[u2_bufid]=0;
+	 }
+	 u2_bufid=0;
+	 /*包含起始位标记
+	 u2_bufid=1;//数据归位清除
+	 u2_res[0]=USART_ReceiveData(USART2);
+	 */
+	 }
+	 else
+	 {
+	 u2_bufid++;
+	 }
+ }
+
+void USART3_IRQHandler(void)
+{
+	 
+	u3_res[u3_bufid]=USART_ReceiveData(USART3);//接收数据
+	 
+	 if (USART_ReceiveData(USART3)==u3_flg)//辨别数据的有效性
+	 {
+		  for(u3_bufid=0;u3_bufid<SerialMAXBUF;u3_bufid++)
+	 {
+		 u3_res[u3_bufid]=0;
+	 }
+	 u3_bufid=0;
+	 /*包含起始位标记
+	 u3_bufid=1;//数据归位清除
+	 u3_res[0]=USART_ReceiveData(USART3);
+	 */
+	 }
+	 else
+	 {
+	 u3_bufid++;
+	 }
+ }
+
+ char* Serialread(u8 SerialID,u8 SFlag)
+ {
+	 switch(SerialID)
+	 {
+		 case 1: u1_flg=SFlag;return u1_res;
+		 case 2: u2_flg=SFlag;return u2_res;
+		 case 3: u3_flg=SFlag;return u3_res;
+	 }
+	 
+ }
+ 
+ void Serialclear(u8 SerialID)
+ {
+	 switch(SerialID)
+	 {
+		  case 1:
+		 {
+			 for(u1_bufid=0;u1_bufid<SerialMAXBUF;u1_bufid++)
+			 {
+				 u1_res[u1_bufid]=0;
+			 }
+			 u1_flg=0;
+			 u1_bufid=0;
+	 
+		 }
+		  case 2:
+		 {
+			 for(u2_bufid=0;u2_bufid<SerialMAXBUF;u2_bufid++)
+			 {
+				 u2_res[u2_bufid]=0;
+			 }
+			 u2_flg=0;
+			 u2_bufid=0;
+	 
+		 }
+		 case 3:
+		 {
+			 for(u3_bufid=0;u3_bufid<SerialMAXBUF;u3_bufid++)
+			 {
+				 u3_res[u3_bufid]=0;
+			 }
+			 u3_flg=0;
+			 u3_bufid=0;
+	 
+		 } 
+	 }
+ }
+ 
+ void attachInterrupt(u16 GPIO_Pin,u8 IPT_Mode,u8 IPT_Pre,u8 IPT_Sub)
+ {
+	 EXTI_InitTypeDef EXTI_InitStructure;
+	 NVIC_InitTypeDef NVIC_InitStructure;
+	 
+	 u8 GPIO_PortSource_x=GPIO_Pin/100-1;
+	 u8 GPIO_PinSource_x=GPIO_Pin-(GPIO_PortSource_x+1)*100;
+	 u32 EXTI_Line_x;
+	 EXTITrigger_TypeDef EXTITri_x;
+	 
+	 switch(GPIO_PinSource_x)
+	 {
+		 case 0:EXTI_Line_x=EXTI_Line0;break;
+		 case 1:EXTI_Line_x=EXTI_Line1;break;
+		 case 2:EXTI_Line_x=EXTI_Line2;break;
+		 case 3:EXTI_Line_x=EXTI_Line3;break;
+		 case 4:EXTI_Line_x=EXTI_Line4;break;
+		 case 5:EXTI_Line_x=EXTI_Line5;break;
+		 case 6:EXTI_Line_x=EXTI_Line6;break;
+		 case 7:EXTI_Line_x=EXTI_Line7;break;
+		 case 8:EXTI_Line_x=EXTI_Line8;break;
+		 case 9:EXTI_Line_x=EXTI_Line9;break;
+		 case 10:EXTI_Line_x=EXTI_Line10;break;
+		 case 11:EXTI_Line_x=EXTI_Line11;break;
+		 case 12:EXTI_Line_x=EXTI_Line12;break;
+		 case 13:EXTI_Line_x=EXTI_Line13;break;
+		 case 14:EXTI_Line_x=EXTI_Line14;break;
+		 case 15:EXTI_Line_x=EXTI_Line15;break;
+	 }
+	 
+	 switch(IPT_Mode)
+	 {
+		 case 0:EXTITri_x=EXTI_Trigger_Rising;break;
+		 case 1:EXTITri_x=EXTI_Trigger_Falling;break;
+		 case 2:EXTITri_x=EXTI_Trigger_Rising_Falling;break;
+	 }
+	 
+	 pinMode(GPIO_Pin,DINPUT);
+	 
+	 RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);	//使能复用功能时钟
+
+   GPIO_EXTILineConfig(GPIO_PortSource_x,GPIO_PinSource_x);
+
+  	EXTI_InitStructure.EXTI_Line=EXTI_Line_x;
+  	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;	
+  	EXTI_InitStructure.EXTI_Trigger = EXTITri_x;
+  	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  	EXTI_Init(&EXTI_InitStructure);	 	//根据EXTI_InitStruct中指定的参数初始化外设EXTI寄存器
+	 
+	  if(GPIO_PinSource_x==0){NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;}
+		if(GPIO_PinSource_x==1){NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;}
+		if(GPIO_PinSource_x==2){NVIC_InitStructure.NVIC_IRQChannel = EXTI2_IRQn;}
+		if(GPIO_PinSource_x==3){NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn;}
+		if(GPIO_PinSource_x==4){NVIC_InitStructure.NVIC_IRQChannel = EXTI4_IRQn;}
+		if((GPIO_PinSource_x>=5)&&(GPIO_PinSource_x<=9)){NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQn;}
+		if((GPIO_PinSource_x>=10)&&(GPIO_PinSource_x<=15)){NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;}
+		
+  	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = IPT_Pre;	//抢占优先级
+  	NVIC_InitStructure.NVIC_IRQChannelSubPriority = IPT_Sub;					//子优先级
+  	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;								//使能外部中断通道
+  	NVIC_Init(&NVIC_InitStructure);
+
+ }
+ 
+ void Interruptclear(u16 GPIO_Pin)
+ {
+	 u32 EXTI_Line_x;
+	 switch(GPIO_Pin-GPIO_Pin/100*100)
+	 {
+		 case 0:EXTI_Line_x=EXTI_Line0;break;
+		 case 1:EXTI_Line_x=EXTI_Line1;break;
+		 case 2:EXTI_Line_x=EXTI_Line2;break;
+		 case 3:EXTI_Line_x=EXTI_Line3;break;
+		 case 4:EXTI_Line_x=EXTI_Line4;break;
+		 case 5:EXTI_Line_x=EXTI_Line5;break;
+		 case 6:EXTI_Line_x=EXTI_Line6;break;
+		 case 7:EXTI_Line_x=EXTI_Line7;break;
+		 case 8:EXTI_Line_x=EXTI_Line8;break;
+		 case 9:EXTI_Line_x=EXTI_Line9;break;
+		 case 10:EXTI_Line_x=EXTI_Line10;break;
+		 case 11:EXTI_Line_x=EXTI_Line11;break;
+		 case 12:EXTI_Line_x=EXTI_Line12;break;
+		 case 13:EXTI_Line_x=EXTI_Line13;break;
+		 case 14:EXTI_Line_x=EXTI_Line14;break;
+		 case 15:EXTI_Line_x=EXTI_Line15;break;
+	 }
+	 EXTI_ClearITPendingBit(EXTI_Line_x);
+ }
+ 
+ void timerInterrupt(TIM_TypeDef* TIMx,u16 tdelta)
+ {
+	 u16 psc=7199;
+	 u16 arr=tdelta*0.001*72*1000000/7200-1;
+	 
+	 TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	 NVIC_InitTypeDef NVIC_InitStructure;
+	 
+	if(TIMx==TIM1){RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1 , ENABLE);}
+	if(TIMx==TIM2){RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 , ENABLE);}
+	if(TIMx==TIM3){RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 , ENABLE);}
+	if(TIMx==TIM4){RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4 , ENABLE);}
+	if(TIMx==TIM5){RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5 , ENABLE);}
+	
+	
+	 //定时器初始化
+	 TIM_TimeBaseStructure.TIM_Period = arr; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值	
+	 TIM_TimeBaseStructure.TIM_Prescaler =psc; //设置用来作为TIMx时钟频率除数的预分频值
+	 TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; //设置时钟分割:TDTS = Tck_tim
+	 TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM向上计数模式
+	 TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;//重复计数设置【警告】TIM1不设置将无法正常工作
+	 TIM_TimeBaseInit(TIMx, &TIM_TimeBaseStructure); //根据指定的参数初始化TIMx的时间基数单位
+	 TIM_ITConfig(TIMx,TIM_IT_Update,ENABLE ); //允许更新中断
+	 
+	 //中断优先级NVIC设置
+	 if(TIMx==TIM1){NVIC_InitStructure.NVIC_IRQChannel = TIM1_UP_IRQn;}
+	 if(TIMx==TIM2){NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;}
+	 if(TIMx==TIM3){NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;}
+	 if(TIMx==TIM4){NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;}
+	 if(TIMx==TIM5){NVIC_InitStructure.NVIC_IRQChannel = TIM5_IRQn;}
+	 
+	 NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  //先占优先级
+	 NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;  //从优先级
+	 NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道被使能
+	 NVIC_Init(&NVIC_InitStructure);  //初始化NVIC寄存器
+	 
+	 TIM_Cmd(TIMx, ENABLE);  //使能TIMx	
+ }
+ 
+
---
+++ EasySTM32F103.h
@@ -0,0 +1,268 @@
+#ifndef __EasySTM32F103_H
+#define __EasySTM32F103_H 
+
+/*
+	【EasySTM32F103库函数说明】
+	本库集成封装了STM32F103系列单片机多种基本功能，简化设置，易于入门，
+	贴近Arduino的编程风格，尤其适合从Arduino进阶进入STM32的学习
+	主要功能有：数字输出、上下拉数字输入、模拟输入、PWM输出、串口通信、
+	数据类型转换、外部中断、定时器中断
+	
+作者：LMR
+版本号：V1.0
+创建日期：2018-4-20
+
+*/
+
+#include "stm32f10x.h"
+
+#include "stdio.h"
+#include "stdlib.h"
+
+//快捷引脚号定义区
+#define A0 100
+#define A1 101
+#define A2 102
+#define A3 103
+#define A4 104
+#define A5 105
+#define A6 106
+#define A7 107
+#define A8 108
+#define A9 109
+#define A10 110
+#define A11 111
+#define A12 112
+#define A13 113
+#define A14 114
+#define A15 115
+#define B0 200
+#define B1 201
+#define B2 202
+#define B3 203
+#define B4 204
+#define B5 205
+#define B6 206
+#define B7 207
+#define B8 208
+#define B9 209
+#define B10 210
+#define B11 211
+#define B12 212
+#define B13 213
+#define B14 214
+#define B15 215
+#define C0 300
+#define C1 301
+#define C2 302
+#define C3 303
+#define C4 304
+#define C5 305
+#define C6 306
+#define C7 307
+#define C8 308
+#define C9 309
+#define C10 310
+#define C11 311
+#define C12 312
+#define C13 313
+#define C14 314
+#define C15 315
+#define D0 400
+#define D1 401
+#define D2 402
+#define D3 403
+#define D4 404
+#define D5 405
+#define D6 406
+#define D7 407
+#define D8 408
+#define D9 409
+#define D10 410
+#define D11 411
+#define D12 412
+#define D13 413
+#define D14 414
+#define D15 415
+#define E0 500
+#define E1 501
+#define E2 502
+#define E3 503
+#define E4 504
+#define E5 505
+#define E6 506
+#define E7 507
+#define E8 508
+#define E9 509
+#define E10 510
+#define E11 511
+#define E12 512
+#define E13 513
+#define E14 514
+#define E15 515
+#define F0 600
+#define F1 601
+#define F2 602
+#define F3 603
+#define F4 604
+#define F5 605
+#define F6 606
+#define F7 607
+#define F8 608
+#define F9 609
+#define F10 610
+#define F11 611
+#define F12 612
+#define F13 613
+#define F14 614
+#define F15 615
+#define G0 700
+#define G1 701
+#define G2 702
+#define G3 703
+#define G4 704
+#define G5 705
+#define G6 706
+#define G7 707
+#define G8 708
+#define G9 709
+#define G10 710
+#define G11 711
+#define G12 712
+#define G13 713
+#define G14 714
+#define G15 715
+
+
+/*引脚归属信息查询函数区
+*/
+GPIO_TypeDef* getGPIOX(u16 GPIO_Pin); //获取GPIO组别，A\B\C\D\E\F\G
+u16 getGPIOP(u16 GPIO_Pin);//获取GPIO引脚号 0~15
+TIM_TypeDef* getPWMTIM(u16 GPIO_Pin);//获取引脚对应的PWM定时器
+u8 getPWMCHN(u16 GPIO_Pin);//获取引脚对应的PWM定时器通道
+
+//输入输出功能区
+
+/*【pinMode函数】
+	说明：设置引脚的使用方式
+	调用：GPIO_Pin 引脚号 如A3
+				GPIO_Mode 使用方式 
+				可选OUTPUT(数字电平输出)、UINPUT(数字电平上拉输入，即浮空高电平)、DINPUT(数字电平下拉输入，即浮空低电平)、AINPUT（模拟输入）
+*/
+#define OUTPUT 1
+#define UINPUT 2
+#define DINPUT 3
+#define AINPUT 4
+void pinMode(u16 GPIO_Pin,u8 GPIO_Mode);
+
+/*【digitalWrite函数】
+	说明：设置引脚的高低电平，HIGH为高，LOW为低
+*/
+#define HIGH 1
+#define LOW 0
+void digitalWrite(u16 GPIO_Pin,u8 DW_Mode);
+
+/*【digitalRead函数】
+	说明：读取引脚的高低电平，返回1为高电平，0为低电平
+*/
+u8 digitalRead(u16 GPIO_Pin);
+
+/*【ADCRead函数】
+		说明：读取模拟值电压原始信号，3.3对应4096
+*/
+u16 ADCRead(u16 GPIO_Pin);
+/*【analogRead函数】
+		说明：读取均值滤波后的模拟值信号，且已映射至0~1023
+*/
+u16 analogRead(u16 GPIO_Pin);
+
+
+//PWM功能区
+
+/*【setPWM函数】
+	说明：设置PWM输出引脚
+				GPIO_Pin 引脚号 如A3
+				PWM_MAX PWM最大值 如255
+				PWM_CMP 反置设置，PPWM为正向，NPWM为反向，这将影响VCC与GND接入方式
+*/
+#define PPWM 1
+#define NPWM 0
+void setPWM(u16 GPIO_Pin,u16 PWM_MAX,u8 PWM_CMP);
+
+/*【analogWrite函数】
+		说明：输出PWM值
+*/
+void analogWrite(u16 GPIO_Pin,u16 PWM_VALUE);
+
+//串口通讯功能区
+
+/*【setSerial函数】
+		说明：设置串口通信
+		SerialID 串口编号，目前支持1~3
+		bound 串口波特率设置
+*/
+void setSerial(u8 SerialID,u32 bound);
+
+/*【Serialwrite函数】
+		说明：串口写入单一数据
+*/
+void Serialwrite(u8 SerialID,char Data);
+
+/*【Serialprint函数】
+		说明：串口发送字符串
+		SerialID 串口编号
+		SData 字符串，注意不支持直接输入整型、浮点型，请用函数转换后再输入
+*/
+void Serialprint(u8 SerialID,char* SData);
+
+/*【Serialread函数】
+		说明：串口接收函数，返回字符串，请用函数转换得到整型、浮点型
+		SerialID 串口编号
+		SFlag 起始位标记号（char）
+*/
+char* Serialread(u8 SerialID,u8 SFlag);
+
+void Serialclear(u8 SerialID);
+
+/*【数据类型转换函数区】
+	说明：为方便串口数据进行转换读取，特提供常用的数据类型转换函数，依赖C标准库"stdio.h"及"stdlib.h"
+	int2str 将u32整型数据转换为字符串，可与Serialprint直接对接，依赖stdio.h
+	flo2str 将float浮点数据转换为字符串，超出部分四舍五入，可与Serialprint直接对接，依赖stdio.h
+	stringtoint 不依赖库自建字符串转整型函数
+	另外库本身提供函数可进行更丰富的转换
+	sprintf 格式化输出，可与Serialprint直接对接实现printf的输出效果，依赖stdio.h
+	atof 将字符串转换为双精度浮点型值，依赖stdlib.h
+	atoi 将字符串转换为双精度整型值，依赖stdlib.h
+	atol 将字符串转换为双精度长整型值，依赖stdlib.h
+*/
+char* int2str(u32 oriint);
+char* flo2str(float oriflo);
+u32 stringtoint(char* str);
+
+//中断设置功能区
+
+/*【attachInterrupt函数】
+		说明：外部中断设置
+		GPIO_Pin 引脚编号
+		IPT_Mode 中断类型，包括RISING（上升沿触发）、FALLING（下降沿触发）、CHANGE（边沿触发）
+		IPT_Pre（抢占优先级）与IPT_Sub（响应优先级）负责管理中断的响应顺序，均为数字越小优先级别越高
+		且IPT_Pre又比IPT_Sub优先级高，同级响应则谁先触发谁响应
+*/
+#define RISING 0
+#define FALLING 1
+#define CHANGE 2
+void attachInterrupt(u16 GPIO_Pin,u8 IPT_Mode,u8 IPT_Pre,u8 IPT_Sub);
+
+/*【Interruptclear函数】
+		说明：中断标志位清除函数，避免重复触发
+*/
+void Interruptclear(u16 GPIO_Pin);
+
+/*【timerInterrupt函数】
+		说明：定时器中断函数
+		TIMx 定时器编号，包括TIM1,TIM2,TIM3,TIM4,TIM5(C8T6无)
+		tdelta 中断间隔时间，以ms为单位
+*/
+void timerInterrupt(TIM_TypeDef* TIMx,u16 tdelta);
+
+#endif
